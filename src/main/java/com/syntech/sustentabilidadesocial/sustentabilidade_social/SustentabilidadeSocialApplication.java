af3f44556f67b6b59f75e4d24f931cdb54303495
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-10-07 18:54:36 -0300

    feat: adiciona projeto gerado pelo spring initalizr
    Signed-off-by: Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>

---
+++ src/main/java/com/syntech/sustentabilidadesocial/sustentabilidade_social/SustentabilidadeSocialApplication.java
@@ -0,0 +1,12 @@
+package com.syntech.sustentabilidadesocial.sustentabilidade_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustentabilidadeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustentabilidadeSocialApplication.class, args);
+	}
+}
---
+++ src/test/java/com/syntech/sustentabilidadesocial/sustentabilidade_social/SustentabilidadeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntech.sustentabilidadesocial.sustentabilidade_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentabilidadeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
