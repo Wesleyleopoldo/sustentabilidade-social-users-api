71dc334ea955489d5b6ebade39c23ac8a9a79619
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-10-08 18:33:52 -0300

    feat: add DTO para receber os dados do usuário na requisição de criação de usuário novo
    Signed-off-by: Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>

---
+++ src/main/java/com/syntech/sustentabilidadesocial/sustentabilidade_social/requests/CreateUserRequest.java
@@ -0,0 +1,4 @@
+package com.syntech.sustentabilidadesocial.sustentabilidade_social.requests;
+
+public record CreateUserRequest(String pictureProfile, String name, String email, String password) {
+}
