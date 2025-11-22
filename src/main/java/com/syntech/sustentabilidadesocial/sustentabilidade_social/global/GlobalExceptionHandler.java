436d3817361f0c86544b257123cce5c3b76f3996
Wesleyleopoldo <desenvolvimentowesleyleopoldo@gmail.com>
2025-11-22 06:06:33 -0300

    feat: add novos tratamentos ao controller advice e altera o de erro interno do servidor
    Signed-off-by: Wesleyleopoldo <desenvolvimentowesleyleopoldo@gmail.com>

--- src/main/java/com/syntech/sustentabilidadesocial/sustentabilidade_social/global/GlobalExceptionHandler.java
+++ src/main/java/com/syntech/sustentabilidadesocial/sustentabilidade_social/global/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.syntech.sustentabilidadesocial.sustentabilidade_social.global;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,18 +12,21 @@ import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.Already
 import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.BadCredentialsException;
 import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.EmailNotValidException;
 import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.InternalErrorException;
+import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.InvalidTokenException;
 import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.NotFoundException;
+import com.syntech.sustentabilidadesocial.sustentabilidade_social.errors.NullPointerException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(InternalErrorException.class)
-    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
-        return ResponseEntity.status(500).body(new ErrorResponse(ex.getMessage(), 500));
+    public ResponseEntity<ErrorResponse> handleGeneralException(InternalErrorException ex) {
+        ex.printStackTrace();
+        return ResponseEntity.status(500).body(new ErrorResponse("Erro inesperado", 500));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex)  {
+    public ResponseEntity<ErrorResponse> handleNotFoundException(Exception ex) {
         return ResponseEntity.status(404).body(new ErrorResponse(ex.getMessage(), 404));
     }
 
@@ -38,4 +44,24 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleBadCredentialsException(Exception ex) {
         return ResponseEntity.status(400).body(new ErrorResponse(ex.getMessage(), 400));
     }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse> handleNullPointerException(Exception ex) {
+        return ResponseEntity.status(400).body(new ErrorResponse(ex.getMessage(), 400));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorResponse>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(400).body(ex.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .map(err -> new ErrorResponse(err.getDefaultMessage(), 400)).toList()
+    );
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidTokenException(Exception ex) {
+        return ResponseEntity.status(403).body(new ErrorResponse(ex.getMessage(), 403));
+    }
+        
 }
